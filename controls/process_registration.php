c09bb91c3136c05c9f8c40cc718c5681f037f973
Abu Sayem <abusayem1299@gmail.com>
2023-10-10 00:28:45 +0600

    Added Lab 3 Task

--- controls/process_registration.php
+++ controls/process_registration.php
@@ -1,67 +1,109 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    // Initialize an array to store validation errors
-    $errors = [];
+    $firstNameError = $lastNameError = $usernameError = $emailError = $dobError = $genderError = $organizationError = $countryError = $phoneError = $bioError = $passwordError = $confirmPasswordError = $profilePictureError = $termsError = "";
+    $hasErrors = false;
 
-    // Check if any field is empty and add errors to the $errors array
-    if (empty($_POST["first_name"])) {
-        $errors[] = "First Name is required.";
-    }
-    if (empty($_POST["last_name"])) {
-        $errors[] = "Last Name is required.";
-    }
-    if (empty($_POST["username"])) {
-        $errors[] = "Username is required.";
-    }
-    if (empty($_POST["email"]) || !filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
-        $errors[] = "Valid Email is required.";
-    }
-    if (empty($_POST["dob"])) {
-        $errors[] = "Date of Birth is required.";
-    }
-    if (empty($_POST["gender"])) {
-        $errors[] = "Gender is required.";
-    }
-    if (empty($_POST["organization"])) {
-        $errors[] = "Organization is required.";
-    }
-    if ($_POST["country"] === "select country") {
-        $errors[] = "Please select a country.";
-    }
-    if (empty($_POST["phone"]) || !is_numeric($_POST["phone"])) {
-        $errors[] = "Valid Phone Number is required.";
-    }
-    if (empty($_POST["bio"])) {
-        $errors[] = "Bio is required.";
-    }
-    if (strlen($_POST["password"]) < 8 || empty($_POST["password"])) {
-        $errors[] = "Password is too short.";
-    }
-    if ($_POST["password"] !== $_POST["confirm_password"]) {
-        $errors[] = "Passwords do not match.";
-    }
-    if ($_FILES["profile_picture"]["error"] === UPLOAD_ERR_OK) {
-        $allowedFormats = ["image/jpeg", "image/jpg", "image/png"];
-        $fileType = $_FILES["profile_picture"]["type"];
-
-        if (!in_array($fileType, $allowedFormats)) {
-            $errors[] = "Profile picture must be in JPG, JPEG, or PNG format.";
+    if ($_SERVER["REQUEST_METHOD"] === "POST") {
+        // Check each field for errors
+        if (empty($_POST["first_name"])) {
+            $firstNameError = "First Name is required.";
+            $hasErrors = true;
         }
-    } else {
-        $errors[] = "Profile picture upload failed. Please try again.";
-    }
-
-    if (empty($_POST["terms"])) {
-        $errors[] = "You must agree to the Terms & Conditions.";
-    }
-
-    // If there are validation errors, display them
-    if (!empty($errors)) {
-        foreach ($errors as $error) {
-            echo "<p>Error: $error</p>";
+        if (empty($_POST["last_name"])) {
+            $lastNameError = "Last Name is required.";
+            $hasErrors = true;
         }
-    } else {
-        echo "Registration Successful";
+        if (empty($_POST["username"])) {
+            $usernameError = "Username is required.";
+            $hasErrors = true;
+        }
+        if (empty($_POST["email"]) || !filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+            $emailError = "Valid Email is required.";
+            $hasErrors = true;
+        }
+        if (empty($_POST["dob"])) {
+            $dobError = "Date of Birth is required.";
+            $hasErrors = true;
+        }
+        if (empty($_POST["gender"])) {
+            $genderError = "Gender is required.";
+            $hasErrors = true;
+        }
+        if (empty($_POST["organization"])) {
+            $organizationError = "Organization is required.";
+            $hasErrors = true;
+        }
+        if ($_POST["country"] === "select country") {
+            $countryError = "Please select a country.";
+            $hasErrors = true;
+        }
+        if (empty($_POST["phone"]) || !is_numeric($_POST["phone"])) {
+            $phoneError = "Valid Phone Number is required.";
+            $hasErrors = true;
+        }
+        if (empty($_POST["bio"])) {
+            $bioError = "Bio is required.";
+            $hasErrors = true;
+        }
+        if (strlen($_POST["password"]) < 8 || empty($_POST["password"])) {
+            $passwordError = "Password is too short.";
+            $hasErrors = true;
+        }
+        if ($_POST["password"] !== $_POST["confirm_password"]) {
+            $confirmPasswordError = "Passwords do not match.";
+            $hasErrors = true;
+        }
+        if ($_FILES["profile_picture"]["error"] === UPLOAD_ERR_OK) {
+            $allowedFormats = ["image/jpeg", "image/jpg", "image/png"];
+            $fileType = $_FILES["profile_picture"]["type"];
+            if (!in_array($fileType, $allowedFormats)) {
+                $profilePictureError = "Profile picture must be in JPG, JPEG, or PNG format.";
+                $hasErrors = true;
+            }
+        } else {
+            $profilePictureError = "Profile picture upload failed. Please try again.";
+            $hasErrors = true;
+        }
+        if (empty($_POST["terms"])) {
+            $termsError = "You must agree to the Terms & Conditions.";
+            $hasErrors = true;
+        }
+        if (!$hasErrors) {
+            $existingData = file_get_contents("../data/admin_data.json");
+            $phpData = json_decode($existingData, true); // Decode as an associative array
+
+            $formdata = array(
+                "FirstName" => $_POST["first_name"],
+                "LastName" => $_POST["last_name"],
+                "Username" => $_POST["username"],
+                "Email" => $_POST["email"],
+                "Dob" => $_POST["dob"],
+                "Gender" => $_POST["gender"],
+                "Organization" => $_POST["organization"],
+                "Country" => $_POST["country"],
+                "Phone" => $_POST["phone"],
+                "Bio" => $_POST["bio"], 
+                "Password" => $_POST["password"],
+            );
+            // Move the uploaded profile picture to a destination folder
+            $uploadDirectory = "../uploads/";
+            $targetFile = $uploadDirectory . $_POST["email"].".jpg";
+
+            if (move_uploaded_file($_FILES["profile_picture"]["tmp_name"], $targetFile)) {
+                // Profile picture uploaded successfully
+                $formdata["ProfilePicture"] = $targetFile;
+            } else {
+                echo "Profile picture upload failed. Please try again.";
+                exit;
+            }
+
+            $phpData[] = $formdata;
+            $jsondata = json_encode($phpData, JSON_PRETTY_PRINT);
+
+            if (file_put_contents("../data/admin_data.json", $jsondata)) {
+                echo "Registration Successful";
+            } else {
+                echo "File write failed";
+            }
+        }
     }
-}
 ?>
--- view/registration.php
+++ view/registration.php
@@ -1,3 +1,7 @@
+<?php 
+    include '..\controls\process_registration.php';
+?>
+
 <!DOCTYPE html> 
 <html> 
 <head> 
@@ -6,26 +10,31 @@
 <body> 
     <h1>Problemsetter Registration</h1>
     <table> 
-        <form action="..\controls\process_registration.php" method="POST" enctype="multipart/form-data"> 
+        <form action="" method="POST" enctype="multipart/form-data"> 
             <tr> 
                 <th><label for="first_name">First Name:</label></th> 
                 <td><input type="text" id="first_name" name="first_name"></td> 
+                <td><?php echo $firstNameError; ?></td> 
             </tr> 
             <tr> 
                 <th><label for="last_name">Last Name:</label></th> 
                 <td><input type="text" id="last_name" name="last_name"></td> 
+                <!-- <td><?php echo $lastNameError; ?></td>  -->
             </tr> 
             <tr> 
                 <th><label for="username">Username:</label></th> 
-                <td><input type="text" id="username" name="username" ></td> 
+                <td><input type="text" id="username" name="username"></td> 
+                <td><?php echo $usernameError; ?></td> 
             </tr> 
             <tr> 
                 <th><label for="email">Email:</label></th> 
                 <td><input type="email" id="email" name="email"></td> 
+                <td><?php echo $emailError; ?></td> 
             </tr> 
             <tr> 
                 <th><label for="dob">Date of Birth</label></th> 
                 <td><input type="date" id="dob" name="dob"></td> 
+                <td><?php echo $dobError; ?></td> 
             </tr>
             <tr> 
                 <th><label for="gender">Gender:</label></th> 
@@ -34,10 +43,12 @@
                    <input type="radio" name="gender" value="female"> Female
                    <input type="radio" name="gender" value="other"> Other
                 </td>
+                <td><?php echo $genderError; ?></td> 
             </tr>
             <tr> 
                 <th><label for="organization">Organization:</label></th> 
                 <td><input type="text" id="organization" name="organization"></td> 
+                <td><?php echo $organizationError; ?></td> 
             </tr> 
             <tr>
                <th><label for="organization">Country:</label></th>
@@ -49,30 +60,37 @@
                        <option value="BD">Bangladesh</option>
                    </select>
                </td>
+               <td><?php echo $countryError; ?></td> 
             </tr>
             <tr> 
                 <th><label for="phone">Phone Number</label></th> 
-                <td><input type="number" id="phone" name="phone"></td> 
+                <td><input type="text" id="phone" name="phone"></td> 
+                <td><?php echo $phoneError; ?></td> 
             </tr>
             <tr> 
                 <th><label for="bio">Bio:</label></th> 
                 <td><textarea id="bio" name="bio" rows="4" cols="21"></textarea></td> 
+                <td><?php echo $bioError; ?></td> 
             </tr> 
             <tr> 
                 <th><label for="password">Password:</label></th> 
                 <td><input type="password" id="password" name="password"></td> 
+                <td><?php echo $passwordError; ?></td> 
             </tr> 
             <tr> 
                 <th><label for="confirm_password">Confirm Password:</label></th> 
                 <td><input type="password" id="confirm_password" name="confirm_password"></td> 
+                <td><?php echo $confirmPasswordError; ?></td> 
             </tr> 
             <tr> 
                 <th><label for="profile_picture">Profile Picture:</label></th> 
                 <td><input type="file" id="profile_picture" name="profile_picture" accept="image/*"></td> 
+                <td><?php echo $profilePictureError; ?></td> 
             </tr> 
             <tr> 
                 <th><label for="terms">Terms & Condition</label></th> 
                 <td><input type="checkbox" id="terms" name="terms">Agree</td> 
+                <td><?php echo $termsError; ?></td> 
             </tr>
             <tr> 
                 <td style="align: center;"><input type="submit" value="Register"></td> 
